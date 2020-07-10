1e5160ade7280abb9e2e85169208e8b37b7af2e9
kisan79 <kumar.kisan79@gmail.com>
2020-07-10 09:27:45 +0530

    AdminMode is created for storing Admin credentials

--- myadmin/models.py
+++ myadmin/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AdminLoginModel(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=15)
+    mobile = models.BigIntegerField()
+    email = models.EmailField()
+    pic = models.ImageField(blank=True,upload_to="admin_pics/")
