259927120564fa9778c55521dbc2faec809cfe9e
kisan79 <kumar.kisan79@gmail.com>
2020-07-10 10:59:51 +0530

    Some Fields are Modified in models and migration and migrate done

--- myadmin/models.py
+++ myadmin/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class AdminLoginModel(models.Model):
-    username = models.CharField(max_length=30)
+    username = models.CharField(max_length=30,unique=True)
     password = models.CharField(max_length=15)
-    mobile = models.BigIntegerField()
-    email = models.EmailField()
+    mobile = models.BigIntegerField(unique=True)
+    email = models.EmailField(unique=True)
     pic = models.ImageField(blank=True,upload_to="admin_pics/")
