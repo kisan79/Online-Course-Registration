1a92b6e7adf696ad214eac2ca18d8d97ef0fba9c
kisan79 <kumar.kisan79@gmail.com>
2020-07-13 17:22:07 +0530

    myadmin Model modified and migrations migrate done

--- myadmin/models.py
+++ myadmin/models.py
@@ -1,11 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# Common Register Model
 
 
-class AdminLoginModel(models.Model):
-    username = models.CharField(max_length=30,unique=True)
+class CommonModel(models.Model):
+    '''Common Register Model'''
+    idno = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=15)
     mobile = models.BigIntegerField(unique=True)
     email = models.EmailField(unique=True)
-    pic = models.ImageField(blank=True,upload_to="admin_pics/")
+    address = models.TextField()
+
+
+    class Meta:
+        abstract = True
+
+
+# Admin Login Model
+
+
+class AdminLoginModel(CommonModel):
+    '''Admin Model'''
+    pic = models.ImageField(blank=True, upload_to="admin_pics/")
+
+# Course Type Model
+
+
+class CourseType(models.Model):
+    '''Different Trending Types Of Courses'''
+    idno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40, unique=True)
+    pic = models.ImageField(blank=True, upload_to="coursetype_logo/")
+
+# Course Model
+
+
+class Course(models.Model):
+    '''Course Model'''
+    idno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40,unique=True)
+    pic = models.ImageField(blank=True, upload_to="course_pics/")
+    type = models.ManyToManyField(CourseType)
+    fee = models.FloatField()
