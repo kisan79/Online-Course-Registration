8bccc66040a3453db91d4d1e89a6e2a690e20fdc
kisan79 <kumar.kisan79@gmail.com>
2020-07-13 09:40:50 +0530

    Admin Login Part Successful with Session

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from myadmin import views
 
-# app_name='admin'
+app_name='myadmin'
 urlpatterns = [
     path('',views.admin_login,name="admin_login"),
+    path('admin_login_check/',views.admin_login_check,name="admin_login_check"),
+    path('login_success/',views.admin_login_success,name="login_success"),
 ]
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import AdminLoginForm
+from .models import AdminLoginModel
+from django.contrib import messages
 
 
 def admin_login(request):
     '''The FBV returns a HttpResponse with AdminLoginForm'''
     return render(request,template_name="myadmin/admin_login.html",context={'AdminLoginForm':AdminLoginForm()})
+
+
+def admin_login_check(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    try:
+        obj = AdminLoginModel.objects.get(username=username,password=password)
+        request.session['status'] = obj.mobile
+        return redirect('myadmin:login_success')
+    except AdminLoginModel.DoesNotExist:
+        messages.error(request,"Invalid Credentials")
+        return redirect('myadmin:admin_login')
+
+
+def admin_login_success(request):
+    return render(request,"myadmin/login_success.html")
