b66e6ad34f7789e89012bad6bd29f3272d08a472
kisan79 <kumar.kisan79@gmail.com>
2020-07-13 17:14:00 +0530

    student and faculty app registered in INSTALLED_APP

--- OnlineCourseReg/settings.py
+++ OnlineCourseReg/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myadmin.apps.MyadminConfig',
+    'student.apps.StudentConfig',
+    'faculty.apps.FacultyConfig',
 ]
 
 MIDDLEWARE = [
