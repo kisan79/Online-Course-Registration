ce1e1d06501de7e15582cb3cf06340672ed4ea9f
kisan79 <kumar.kisan79@gmail.com>
2020-07-14 00:21:08 +0530

    CourseTypeForm and CourseForm created

--- myadmin/forms.py
+++ myadmin/forms.py
@@ -1,13 +1,11 @@
-#Admin form
-
-
 from django import forms
-from .models import AdminLoginModel
+from .models import *
+
+# Admin form
 
 
 class AdminLoginForm(forms.ModelForm):
-    '''Admin Login Form'''
-
+    """Admin Login Form"""
     class Meta:
         model = AdminLoginModel
         fields = ('username','password')
@@ -25,3 +23,69 @@ class AdminLoginForm(forms.ModelForm):
                 }
             )
         }
+
+
+# CourseType Form
+
+class CourseTypeForm(forms.ModelForm):
+    """CourseType Form"""
+    class Meta:
+        model = CourseType
+        fields = "__all__"
+        exclude = ("idno",)
+    name = forms.CharField(
+        label="Course Type",
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control-lg",
+                "placeholder":"Course Type Name"
+            }
+        )
+    )
+    pic = forms.ImageField(
+        label="Logo",
+        required=False,
+        widget=forms.FileInput(
+            attrs={
+                "class":"form-control-md",
+            }
+        )
+    )
+
+# Course Form
+
+
+class CourseForm(forms.ModelForm):
+    """Course Form"""
+    class Meta:
+        model = Course
+        fields = "__all__"
+        exclude = ("idno",)
+
+    name = forms.CharField(
+        label= 'Course Name',
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control-lg",
+                "placeholder":"Course Name"
+            }
+        )
+    )
+
+    pic = forms.ImageField(
+        label="Image",
+        required=False,
+        widget=forms.FileInput(
+            attrs={
+                "class": "form-control-md",
+            }
+        )
+    )
+
+    fee = forms.FloatField(
+        widget=forms.NumberInput(
+            attrs={
+                "class":"form-control-lg"
+            }
+        )
+    )
