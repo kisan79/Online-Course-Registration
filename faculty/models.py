4f4dfcfb1cd23a1dd559e9099460a08409f2fea2
kisan79 <kumar.kisan79@gmail.com>
2020-07-14 09:22:10 +0530

    Faculty Model modified due to ImportError :Common Model Can't able to inherit

--- faculty/models.py
+++ faculty/models.py
@@ -1,11 +1,19 @@
 from django.db import models
-from myadmin.models import CommonModel
+# from myadmin.models import CommonModel
 
 
 # Faculty Model
 
 
-class Faculty(CommonModel):
+class Faculty(models.Model):
     '''Faculty Model'''
+    idno = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=15)
+    mobile = models.BigIntegerField(unique=True)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
     joined_date = models.DateField(auto_now=True)
     pic = models.ImageField(blank=True, upload_to="faculty_pics/")
