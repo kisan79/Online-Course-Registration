fcb5c8d8ea104cd13a9e831d21d47207884db003
kisan79 <kumar.kisan79@gmail.com>
2020-07-15 08:16:01 +0530

    Student Login Form Created

--- student/forms.py
+++ student/forms.py
@@ -9,3 +9,26 @@ class StudentRegister(forms.ModelForm):
         model = Student
         fields = "__all__"
 
+
+# Student login form
+
+
+class StudentLoginForm(forms.ModelForm):
+    """Admin Login Form"""
+    class Meta:
+        model = Student
+        fields = ('username','password')
+        widgets = {
+            "username":forms.TextInput(
+                attrs={
+                    "class":"form-control-lg",
+                    "placeholder":"Enter Your Username"
+                }
+            ),
+            "password":forms.PasswordInput(
+                attrs={
+                    "class":"form-control-lg",
+                    "placeholder":"Enter Your Password"
+                }
+            )
+        }
