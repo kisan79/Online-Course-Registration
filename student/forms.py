612f6dfd480ea55d714e34d5597bdb23513346f7
kisan79 <kumar.kisan79@gmail.com>
2020-07-21 23:32:30 +0530

    Bootstrap classes added to the form

--- student/forms.py
+++ student/forms.py
@@ -9,22 +9,50 @@ class StudentRegister(forms.ModelForm):
         model = Student
         fields = "__all__"
         widgets ={
+            "first_name":forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "last_name": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "password": forms.PasswordInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
             "username":forms.TextInput(
                 attrs={
+                    "class":"form-control",
                     "onblur":"ajaxCall('id_username','http://127.0.0.1:8000/student/check-username/','msgUsername')",
                 }
             ),
             "mobile":forms.NumberInput(
                 attrs={
+                    "class": "form-control",
                     "type":"tel",
                     "onblur": "ajaxCall('id_mobile','http://127.0.0.1:8000/student/check-mobile/','msgMobile')",
                 }
             ),
             "email":forms.EmailInput(
                 attrs={
+                    "class": "form-control",
                     "onblur": "ajaxCall('id_email','http://127.0.0.1:8000/student/check-email/','msgEmail')",
                 }
-            )
+            ),
+            "address": forms.Textarea(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
+            "pic": forms.FileInput(
+                attrs={
+                    "class": "form-control",
+                }
+            ),
         }
 
 
